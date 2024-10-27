ef81ed01b1b7575c50c5ab4b09f0bfedd64ac8d1
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-27 12:39:39 +0200

    Hangman 7-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,34 +2,37 @@ import random
 
 
 def hangman_ver5():
-    print("HANGMAN")
-    words = ['python', 'java', 'javascript', 'php']
-    word = random.choice(words)
-    guessed = ['-' for _ in word]
-    attempts_left = 8
-    guessed_letters = set()
+        print("HANGMAN")
+        words = ['python', 'java', 'javascript', 'php']
+        word = random.choice(words)
+        guessed = ['-' for _ in word]
+        attempts_left = 8
+        guessed_letters = set()
 
-    while attempts_left > 0 and '-' in guessed:
-        print(''.join(guessed))
-        letter = input("Input a letter: > ").strip().lower()
+        while attempts_left > 0 and '-' in guessed:
+            print(''.join(guessed))
+            letter = input("Input a letter: > ").strip()
 
-        if letter in guessed_letters:
-            print("No improvements")
-            attempts_left -= 1
-        elif letter in word:
-            guessed_letters.add(letter)
-            for i, l in enumerate(word):
-                if l == letter:
-                    guessed[i] = letter
+            if len(letter) != 1:
+                print("You should input a single letter")
+            elif not letter.islower():
+                print("Please enter a lowercase English letter")
+            elif letter in guessed_letters:
+                print("You've already guessed this letter")
+            elif letter in word:
+                guessed_letters.add(letter)
+                for i, l in enumerate(word):
+                    if l == letter:
+                        guessed[i] = letter
+            else:
+                guessed_letters.add(letter)
+                attempts_left -= 1
+                print("That letter doesn't appear in the word")
+
+        if '-' not in guessed:
+            print("You survived!")
         else:
-            guessed_letters.add(letter)
-            attempts_left -= 1
-            print("That letter doesn't appear in the word")
-
-    if '-' not in guessed:
-        print("You survived!")
-    else:
-        print("You lost!")
+            print("You lost!")
 
 
 hangman_ver5()
