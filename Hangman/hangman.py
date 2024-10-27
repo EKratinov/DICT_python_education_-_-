d65d0c96de72a3a25d1d41151f6bee1a133c13ae
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-27 12:19:56 +0200

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,15 +1,17 @@
 import random
 
 
-def hangman_ver2():
+def hangman_ver3():
     print("HANGMAN")
     words = ['python', 'java', 'javascript', 'php']
     word = random.choice(words)
-    guess = input("Guess the word: > ").strip().lower()
+    hint = word[:3] + '-' * (len(word) - 3)
+    print(f"Guess the word {hint}: > ", end="")
+    guess = input().strip().lower()
     if guess == word:
         print("You survived!")
     else:
         print("You lost!")
 
 
-hangman_ver2()
+hangman_ver3()
