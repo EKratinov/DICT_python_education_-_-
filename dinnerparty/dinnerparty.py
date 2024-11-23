8d86f88a5b4ff7a4faf12a8b3b7ee498de19594b
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-11-23 14:29:54 +0200

    DinnerParty 4-th stage fix1

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,52 +1,69 @@
 import random
 
 
+def get_number_of_friends():
+    while True:
+        try:
+            num_of_people = int(input("Enter the number of friends joining (including you): "))
+            if num_of_people <= 0:
+                print("No one is joining for the party")
+                return 0
+            return num_of_people
+        except ValueError:
+            print("Please enter a valid number.")
+
+
 def add_friends():
-    num_of_friends = int(input("Enter the number of friends joining (including you): "))
-    if num_of_friends <= 0:
-        print("No one is joining for the party")
+    num_of_friends = get_number_of_friends()
+    if num_of_friends == 0:
         return {}, 0
-    else:
-        friends = {}
-        for _ in range(num_of_friends):
-            name = input("Enter the name of every friend (including you), each on a new line: ")
-            friends[name] = 0
-        print("Friends list:", friends)
-        return friends, num_of_friends
+
+    friends_dict = {}
+    for _ in range(num_of_friends):
+        name = input("Enter the name of every friend (including you), each on a new line: ").strip()
+        friends_dict[name] = 0
+    print("Friends list:", friends_dict)
+    return friends_dict, num_of_friends
 
 
-def split_bill(friends, num_of_friends):
-    if num_of_friends > 0:
-        total_amount = float(input("Enter the total amount: "))
-        split_amount = round(total_amount / num_of_friends, 2)
-        for friend in friends:
-            friends[friend] = split_amount
-        print("Updated friends list with split amounts:", friends)
-    return friends, total_amount
-    return friends, 0
+def split_bill(friends_dict, num_friends):
+    if total_friends > 0:
+        while True:
+            try:
+                total_bill = float(input("Enter the total amount: "))
+                break
+            except ValueError:
+                print("Please enter a number.")
+        split_amount = round(total_bill / num_friends, 2)
+        for friend in friends_dict:
+            friends_dict[friend] = split_amount
+        print("Updated friends list with split amounts:", friends_dict)
+        return friends_dict, total_bill
+    return friends_dict, 0
 
 
-def choose_lucky(friends):
-    lucky_choice = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ')
-    if lucky_choice in ["Yes", "yes", "yes ", "Yes "]:
-        lucky_friend = random.choice(list(friends.keys()))
-        print(f"{lucky_friend} is the lucky one!")
-        return lucky_friend
+def choose_lucky(friends_dict):
+    lucky_choice = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ').strip()
+    if lucky_choice.lower() == "yes":
+        lucky_person = random.choice(list(friends_dict.keys()))
+        print(f"{lucky_person} is the lucky one!")
+        return lucky_person
     else:
         print("No one is going to be lucky")
         return None
 
 
-def recalculate(friends, lucky_friend, total_amount):
+def recalculate_bill(friends_dict, lucky_friend, bill_amount):
     if lucky_friend:
-        new_split_amount = round(total_amount / (len(friends) - 1), 2)
-        for friend in friends:
-            friends[friend] = new_split_amount if friend != lucky_friend else 0
-    print("Final friends list with recalculated amounts:", friends)
-    return friends
+        new_split_amount = round(bill_amount / (len(friends_dict) - 1), 2)
+        for friend in friends_dict:
+            friends_dict[friend] = new_split_amount if friend != lucky_person else 0
+    print("Final friends list with recalculated amounts:", friends_dict)
+    return friends_dict
 
 
-friends, num_of_friends = add_friends()
-friends, total_amount = split_bill(friends, num_of_friends)
-lucky_friend = choose_lucky(friends)
-friends = recalculate(friends, lucky_friend, total_amount)
+friends, total_friends = add_friends()
+if total_friends > 0:
+    friends, total_bill = split_bill(friends, total_friends)
+    lucky_person = choose_lucky(friends)
+    friends = recalculate_bill(friends, lucky_person, total_bill)
