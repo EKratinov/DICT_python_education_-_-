ae8f8c0e09cf9104c8a4361a79841899d3dc8c1c
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-12-13 12:13:52 +0200

    Pencils_game 2-nd stage

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -22,5 +22,31 @@ def initialize_game():
     return pencils, first_player
 
 
+def player_turn(pencils, current_player):
+    while True:
+        print(f"{current_player}'s turn!")
+        move = input("> ")
+        if not move.isdigit() or int(move) not in [1, 2, 3]:
+            print("Possible values: '1', '2' or '3'")
+            continue
+        move = int(move)
+        if move > pencils:
+            print("Too many pencils were taken")
+            continue
+        break
+
+    pencils -= move
+    print("|" * pencils)
+    return pencils
+
+
 if __name__ == "__main__":
     pencils, current_player = initialize_game()
+
+    while pencils > 0:
+        pencils = player_turn(pencils, current_player)
+        current_player = "Name1" if current_player == "Name2" else "Name2"
+
+        if pencils == 0:
+            print(f"{current_player} won!")
+            break
